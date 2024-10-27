d86af0729ec349880ac3692a9f824d37fcd3e059
Nombiembre <emanuelt.marin9@gmail.com>
2024-10-26 19:13:39 -0500

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-import typographyPlugin from '@tailwindcss/typography';
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [typographyPlugin],
+  plugins: [],
 };
